58cdbb4e805ead18f2d96baadb5d339e21d4439a
Vinicius Mazza <viniciusmazza2552@gmail.com>
2020-10-19 12:37:25 -0300

    Criando rotas e adicionando alguns estilos basicos...

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import GlobalStyles from './styles/GlobalStyles'; 
+import Header from './components/Header';
+import Profile from './pages/Profile';
+import Repo from './pages/Repo';
 
 function App() {
   return (
-    <h1>Inicio do Projeto!</h1>
+    <BrowserRouter>
+      <Header />
+
+      <h1>Inicio do Projeto!</h1>
+
+      <Routes>
+        <Route path="/" element={ <Profile /> } />
+        <Route path="/:username" element={ <Profile /> } />
+        <Route path="/:username/:reponame" element={ <Repo /> } />
+      </Routes>
+
+      {/* <Footer /> */}
+      <GlobalStyles />
+    </BrowserRouter>
   );
 }
 
