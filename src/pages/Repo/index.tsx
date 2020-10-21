d7ff90144a0c4b87f8f201af9ccc59b8fc94404f
Vinicius Mazza <viniciusmazza2552@gmail.com>
2020-10-21 11:32:40 -0300

    Criando a pagina repo

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from './styles';
 
 const Repo: React.FC = () => {
-  return <div />;
+  return(
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+
+        <Link className={'username'} to={'/Vinicius-Mazza'}>
+          Vinicius-Mazza
+        </Link>
+
+        <span></span>
+
+        <Link className={'reponame'} to={'/Vinicius-Mazza/github-clone'}>
+          github-clone
+        </Link>
+      
+      </Breadcrumb>
+
+      <p>A simple clone of the github!</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>0</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={'https://github.com/Vinicius-Mazza/github-clone'}>
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
+  );
 }
 
 export default Repo;
