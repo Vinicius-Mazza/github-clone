7494c03190184aede88c3c4399fbb99c9548855f
Vinicius Mazza <viniciusmazza2552@gmail.com>
2020-10-21 17:03:55 -0300

    Criando a navegação (search)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Container, GitHubLogo, SearchForm } from './styles';
 
 const Header: React.FC = () => {
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    navigate('/' + search.toLowerCase().trim());
+  }
+
   return(
     <Container>
       <GitHubLogo />
-      <SearchForm>
-        <input placeholder="Search or jump to..." />
+      <SearchForm onSubmit={handleSubmit}>
+        <input placeholder="Search or jump to..." 
+        value={search} 
+        onChange={e => setSearch(e.currentTarget.value)} 
+      />
       </SearchForm>
     </Container>
   );
